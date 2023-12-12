2aba7b809153557bcdd0b29acb559e53f9d6d977
Zharkyn Markabayev <Lucky_zharkyn@mail.ru>
2023-12-12 09:35:59 +0600

    level 14 end

--- src/codeMu/level14.java
+++ src/codeMu/level14.java
@@ -1,18 +1,34 @@
 package codeMu;
 
+import java.util.Arrays;
+
 public class level14 {
     public static void main(String[] args) {
         //  №1 Дан массив с числами:
-        // int[] arr = [1, 2, 3, 4, 5];
+//         int[] arr = {1, 2, 3, 4, 5};
         // Найдите сумму квадратных корней элементов этого массива.
+//        int sum = 0;
+//        for(int i = 0; i < arr.length; i++) {
+//            sum += Math.sqrt(arr[i]);
+//        }
+//        Helper.show(sum);
 
         // №2 Дан массив с числами:
-        // int[] {1, 2, 3, 4};
-        // Увеличьте каждый элемент этого массива в два раза:
+//         int[] arr = {1, 2, 3, 4};
+//        // Увеличьте каждый элемент этого массива в два раза:
+//
+//        for(int i = 0; i < arr.length; i++) {
+//            arr[i] = arr[i] * 2;
+//        }
+//        Helper.show(Arrays.toString(arr));
+//         int[] arr = {2, 4, 9, 8};
 
-        // int[] {2, 4, 9, 8};
         // №3 Дан массив:
-        // int[10] arr;
-        // С помощью цикла заполните этот массив целыми числами от 10 до 1.
+//         int[] arr = new int[10];
+//        // С помощью цикла заполните этот массив целыми числами от 10 до 1.
+//        for(int i = 0; i < 10; i++) {
+//            arr[i] = 10 - i;
+//        }
+//        Helper.show(Arrays.toString(arr));
     }
 }
