fb61bdb3bad90508e704c0db8fa1ea8c5bbaffe2
Zharkyn Markabayev <Lucky_zharkyn@mail.ru>
2023-12-13 09:55:34 +0600

    level 17 end

---
+++ src/codeMu/level2/level17.java
@@ -0,0 +1,37 @@
+package codeMu.level2;
+
+import codeMu.Helper;
+
+import java.util.Arrays;
+
+public class level17 {
+    public static void main(String[] args) {
+//        // №1 /Дан массив с целыми числами:
+//        int[] arr = {1, 2, 3, 4, 5};
+//        //Найдите сумму квадратов элементов этого массива.
+//        int sum = 0;
+//        for(int elem : arr) {
+//          sum += elem * elem;
+//        }
+//        Helper.show(sum);
+
+        //№2 Дан массив с числами:
+//        int[] arr = {1, 2, 3, 4, 5};
+//        //Слейте элементы этого массива в строку:
+//        //"12345"
+//        StringBuilder stringBuilder = new StringBuilder();
+//        for(int elem : arr) {
+//            stringBuilder.append(elem);
+//        }
+//        Helper.show(stringBuilder.toString());
+
+        //№3 Дан массив с датой:
+//        String[] arr = {"2025", "12", "31"};
+//        //Из элементов этого массива соберите дату в следующем формате:
+//        //"31-12-2025"
+//        StringBuilder stringBuilder = new StringBuilder();
+//        stringBuilder.append(arr[2]).append("-").append(arr[1]).append("-").append(arr[0]);
+//        Helper.show(stringBuilder.toString());
+
+    }
+}
