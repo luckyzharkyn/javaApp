7a91bbec9ca3827df2fb0c374f1c6f1e7c7e8ca6
Zharkyn <Lucky_zharkyn@mail.ru>
2023-12-18 15:32:13 +0600

    level25 start

---
+++ src/codeMu/level3/level24.java
@@ -0,0 +1,56 @@
+package codeMu.level3;
+
+import codeMu.Helper;
+
+import javax.security.auth.callback.CallbackHandler;
+import java.util.Arrays;
+
+public class level24 {
+    public static void main(String[] args) {
+        //    №1 Дан массив с числами
+//            int[] arr = {1, -2, -3, 4, -5, 6};
+//        //    Подсчитайте количество отрицательных чисел в этом массиве.
+//        int count = 0;
+//            for(int i = 0; i < arr.length; i++) {
+//                if(arr[i] < 0) {
+//                    count++;
+//                }
+//            }
+//        Helper.show(count);
+        //    №2 Дано некоторое целое число:
+//            int num = 1257;
+//        //    Проверьте, что все цифры этого числа являются нечетными.
+//            char[] charArray = String.valueOf(num).toCharArray();
+//            for(char elem : charArray) {
+//                if(Character.getNumericValue(elem) % 2 == 0) {
+//                    Helper.show("Has");
+//                    break;
+//                }
+//            }
+
+        //    №3 Дано некоторое число:
+//            int e = 12345;
+//        //    Получите массив цифр этого числа:
+//        //    byte[] {1, 2, 3, 4, 5}
+//            String s = String.valueOf(e);
+//            byte[] arrays = new byte[s.length()];
+//            for(int i = 0; i < s.length(); i++) {
+//                arrays[i] = (byte) s.charAt(i);
+//            }
+//            Helper.show(Arrays.toString(arrays));
+
+        //    №4 Даны два массива:
+//            int[] arr1 = {1, 2, 3};
+//            int[] arr2 = {4, 5, 6};
+////            Объедините эти массивы в один:
+//        //    int[] {1, 2, 3, 4, 5, 6}
+//            int[] res = new int[arr1.length + arr2.length];
+//            for(int i = 0; i < arr1.length; i++) {
+//                res[i] = arr1[i];
+//            }
+//            for(int i = 0; i < arr2.length; i++) {
+//                res[i + arr1.length] = arr2[i];
+//            }
+//            Helper.show(Arrays.toString(res));
+    }
+}
