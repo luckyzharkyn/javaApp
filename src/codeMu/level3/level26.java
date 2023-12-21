f8f0c514869d758c4d7fb4f940c74d424eab1d0e
Zharkyn Markabayev <Lucky_zharkyn@mail.ru>
2023-12-21 10:03:54 +0600

    level 28 start

---
+++ src/codeMu/level3/level26.java
@@ -0,0 +1,36 @@
+package codeMu.level3;
+
+import codeMu.Helper;
+
+public class level26 {
+    public static void main(String[] args) {
+        // №1 Дан массив с числами:
+//        int[] arr = {-1, 2, -3, 4, 5, 11};
+//        //Найдите сумму тех элементов этого массива, которые больше нуля и меньше десяти.
+//        int sum = 0;
+//        for(int elem : arr) {
+//            if(elem > 0 && elem < 10) {
+//                sum += elem;
+//            }
+//        }
+//        Helper.show(sum);
+        //№2 Дан массив с целыми числами:
+//         int[] arr = {1, 2, 3, 4, 5};
+//        //Выведите в консоль элементы этого массива, которые являются четными числами.
+//        for(int elem : arr) {
+//            if(elem % 2 == 0) {
+//                Helper.show(elem);
+//            }
+//        }
+
+        //№3 Дан массив с целыми числами:
+//        int[] arr = {1, 2, 3, 0, 4, 5};
+//        //Выведите в консоль элементы этого массива до первого нуля.
+//        for(int elem : arr) {
+//            Helper.show(elem);
+//            if(elem == 0) {
+//                break;
+//            }
+//        }
+    }
+}
---
+++ src/codeMu/level3/level27.java
@@ -0,0 +1,39 @@
+package codeMu.level3;
+
+import codeMu.Helper;
+
+public class level27 {
+    public static void main(String[] args) {
+        //    №1 Дан массив с целыми числами:
+//            int[] arr = {1, 2, 3, 4, 5};
+//        //    Найдите сумму квадратов элементов этого массива.
+//            int sum = 0;
+//            for(int elem : arr) {
+//                sum += Math.pow(elem, 2);
+//            }
+//            Helper.show(sum);
+        //    №2 Дан массив с числами:
+//            int[] arr = {1, 2, 3, 4, 5};
+//        //    Слейте элементы этого массива в строку:
+//        //    "12345"
+//            StringBuilder stringBuilder = new StringBuilder();
+//            for(int elem : arr) {
+//                stringBuilder.append(elem);
+//            }
+//            Helper.show(stringBuilder.toString());
+        //    №3 Дан массив с датой:
+//            String[] arr = {"2025", "12", "31"};
+//        //    Из элементов этого массива соберите дату в следующем формате:
+//        //    "31-12-2025"
+////            String result = String.join("-", arr[2], arr[1], arr[0]);
+//
+//            StringBuilder stringBuilder = new StringBuilder();
+//            for(int i = arr.length - 1; i >= 0; i--) {
+//                stringBuilder.append(arr[i]);
+//                if(i != 0) {
+//                    stringBuilder.append("-");
+//                }
+//            }
+//            Helper.show(stringBuilder.toString());
+    }
+}
