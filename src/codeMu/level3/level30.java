f9347f3fc10a3c1c914d63a52458815cdd6e063f
Zharkyn Markabayev <Lucky_zharkyn@mail.ru>
2023-12-25 10:29:28 +0600

    level 30 end

--- src/codeMu/level3/level30.java
+++ src/codeMu/level3/level30.java
@@ -1,20 +1,59 @@
 package codeMu.level3;
 
+import codeMu.Helper;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class level30 {
     public static void main(String[] args) {
-        //    №1 Дано целое число:
-        //    int num = 10203304506;
-        //    Выведите в консоль позиции всех цифр 0 в этом числе, за исключением первой и последней.
-
+//        //    №1 Дано целое число:
+//              long num = 10203304506L;
+//        //    Выведите в консоль позиции всех цифр 0 в этом числе, за исключением первой и последней.
+//              String[] array = String.valueOf(num).split("");
+//              ArrayList<Integer> arrayList = new ArrayList<>();
+//              for(int i = 0; i < array.length; i++) {
+//                  if(array[i].equals("0")) {
+//                      arrayList.add(i);
+//                  }
+//              }
+//              arrayList.remove(0);
+//              arrayList.remove(arrayList.size() - 1);
+//              for(int i : arrayList) {
+//                  Helper.show(i);
+//              }
         //    №2 Дан массив с целыми числами:
-        //    int arr1[] = {123, 334, 456, 556, 789};
-        //    Запишите в новый массив числа, состоящие из разных цифр:
-        //    int arr2[] = {123, 456, 789};
+//            int arr1[] = {123, 334, 456, 556, 789};
+//        //    Запишите в новый массив числа, состоящие из разных цифр:
+//        //    int arr2[] = {123, 456, 789};
+//            ArrayList<Integer> arrayList = new ArrayList<>();
+//            for(int i : arr1) {
+//                char[] charArray = String.valueOf(i).toCharArray();
+//                Set<Character> characters = new HashSet<>();
+//                for(char j : charArray) {
+//                    characters.add(j);
+//                }
+//                if(characters.size() == 3) {
+//                    arrayList.add(i);
+//                }
+//            }
+//            int[] arr2 = new int[arrayList.size()];
+//            for(int i = 0; i < arrayList.size(); i++) {
+//                arr2[i] = arrayList.get(i);
+//            }
+//            Helper.show(Arrays.toString(arr2));
 
         //    №3 Дано дробное число:
-        //    float num = 12.34;
-        //    Запишите целую часть числа в одну переменную, а дробную часть - в другую:
-        //    int num1 = 12;
-        //    int num1 = 34;
+//            float num = 12.34f;
+//        //    Запишите целую часть числа в одну переменную, а дробную часть - в другую:
+//        //    int num1 = 12;
+//        //    int num1 = 34;
+//            String[] s = String.valueOf(num).split("\\.");
+//            int num1 = Integer.parseInt(s[0]);
+//            int num2 = Integer.parseInt(s[1]);
+//            Helper.show(num1);
+//            Helper.show(num2);
     }
 }
