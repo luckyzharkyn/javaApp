2d7e742510399c87467964f6c57bf286baef95d3
Zharkyn <Lucky_zharkyn@mail.ru>
2024-01-03 08:49:54 +0600

    level41 start

--- src/codeMu/level5/level41.java
+++ src/codeMu/level5/level41.java
@@ -1,20 +1,36 @@
 package codeMu.level5;
 
+import codeMu.Helper;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class level41 {
     public static void main(String[] args) {
         //№1 Попросите у пользователя ввести через консоль два числа. Выведите сумму этих чисел.
 
         //№2 Дано число:
-        //int num = 133;
-        //Определите, есть ли среди цифр этого числа одинаковые.
+//        int num = 133;
+//        //Определите, есть ли среди цифр этого числа одинаковые.
+//        String s = String.valueOf(num);
+//        boolean result = s.chars().anyMatch(c -> s.chars().filter(x -> x == c).count() > 1);
+//        if(result) {
+//            Helper.show("yes");
+//        }
+
 
         //№3 Дан массив:
-        //int[][] arr = {
-        //{1, 2, 3},
-        //{4, 5, 6},
-        //{7, 8, 9},
-        //};
-        //Выведите в консоль все элементы этого массива.
+//        int[][] arr = {
+//        {1, 2, 3},
+//        {4, 5, 6},
+//        {7, 8, 9},
+//        };
+//        //Выведите в консоль все элементы этого массива.
+//        Arrays.stream(arr)
+//                .flatMapToInt(Arrays::stream)
+//                .forEach(Helper::show);
 
         //№4 Напишите программу, которая выведет в консоль следующую пирамидку:
         //x
