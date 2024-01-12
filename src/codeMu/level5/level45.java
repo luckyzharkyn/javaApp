ca20f8144e93781a4ace74aa38337f86615d3c61
Zharkyn Markabayev <Lucky_zharkyn@mail.ru>
2024-01-12 16:10:17 +0600

    level45 start

--- src/codeMu/level5/level45.java
+++ src/codeMu/level5/level45.java
@@ -1,9 +1,18 @@
 package codeMu.level5;
 
+import codeMu.Helper;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
+
 public class level45 {
     public static void main(String[] args) {
         //№1 Попросите у пользователя ввести через консоль целое число из промежутка от 1 до 10. Если будет введено что-то другое, то попросите пользователя ввести число еще раз.
-
+//        Helper.show("Введите число от 1 до 10");
+//        Scanner scanner = new Scanner(System.in);
+//        EnterFigure(scanner);
         //№2
         //Сформируйте с помощью циклов следующий массив:
         //int[] {
@@ -13,6 +22,15 @@ public class level45 {
         //    {1, 2, 3},
         //    {1, 2, 3},
         //}
+//       int[][] ints = new int[5][3];
+//       for(int i = 0; i < 5; i++) {
+//           for(int j = 0; j < 3; j++) {
+//               ints[i][j] = j + 1;
+//           }
+//       }
+//       for(int[] elem : ints) {
+//           Helper.show(Arrays.toString(elem));
+//       }
 
         //№3 Дана некоторая строка:
         //"abcdeabc"
@@ -24,4 +42,14 @@ public class level45 {
         //Переведите в верхний регистр все нечетные буквы этой строки. В нашем случае должно получится следующее:
         //"AbCdE"
     }
+    static void EnterFigure(Scanner scanner) {
+        if(scanner.hasNextInt()) {
+            Helper.show("yes baby");
+        } else {
+            Helper.show("Введите число еще раз");
+            scanner.next();
+            EnterFigure(scanner);
+        }
+    }
 }
+
