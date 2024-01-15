a7389f4f83491555b68c262f20bcfe0d2d3e412f
Zharkyn <Lucky_zharkyn@mail.ru>
2024-01-15 22:08:41 +0600

    level46 start

---
+++ src/codeMu/level5/level46.java
@@ -0,0 +1,48 @@
+package codeMu.level5;
+
+import codeMu.Helper;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class level46 {
+    public static void main(String[] args) {
+        //№1 Напишите программу, которая будет находить корни квадратного уравнения.
+        // Для этого спросите у пользователя три числа, которые будут коэффициентами уравнения.
+
+        //№2 Сформируйте с помощью циклов следующий массив:
+        //int[] {
+        //    {1, 2, 3},
+        //    {4, 5, 6},
+        //    {7, 8, 9},
+        //}
+//        int count = 1;
+//        int[][] ints = new int[3][3];
+//        for(int i = 0; i < 3; i++) {
+//            for(int j = 0; j < 3; j++) {
+//                ints[i][j] = count;
+//                count++;
+//            }
+//        }
+//        for(int i = 0; i < ints.length; i++) {
+//            Helper.show(Arrays.toString(ints[i]));
+//        }
+
+        //№3 Даны два числа:
+//        int num1 = 12345;
+//        int num2 = 45678;
+//        //Выведите в консоль цифры, которые есть и в одном, и в другом числе.
+//        String[] s1 = Integer.toString(num1).split("");
+//        String[] s2 = Integer.toString(num2).split("");
+//        Set<String> set = new HashSet<>();
+//        for(String s : s1) {
+//            for(String e : s2) {
+//                if(s.equals(e)) {
+//                    set.add(s);
+//                }
+//            }
+//        }
+//        Helper.show(set.toString());
+    }
+}
